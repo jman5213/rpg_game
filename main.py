90c64bc02bf0d2ded7aeb68a0cd8a108980e8b95
jman5213 <jman5213@outlook.com>
2021-12-08 19:56:43 +0000

    added introduction

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Hello world')
+print('''
+Explorer Game
+V2.0
+Created By:
+Jman5213
+--------------
+Instructions:
+Type: 
+Open Moves
+A compass is in
+the top left corner.
+Use it to find 
+your way.''')
